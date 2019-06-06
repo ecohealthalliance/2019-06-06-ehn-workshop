62b38497daae37164040306654dd9cdec26ded1e
Noam Ross <ross@ecohealthalliance.org>
2019-06-05 22:05:59 -0400

    Code intro cleanup

--- ehn-code.R
+++ ehn-code.R
@@ -1,16 +1,14 @@
 # Hello! Welcome to the EcoHealth Net Modeling Workshop
 #
 # I'm your instructor, Dr. Noam Ross of EcoHealth Alliance.
-# We'll walk through this code today.
 #
 # Launch the work environment by clicking https://bit.ly/ehn2019
 #
-# Code will be updated as I type heree: http://bit.ly/ehn2019b
+# Code will be updated as I type here: http://bit.ly/ehn2019b
 #
 # Questions after the workshop? Contact me at ross@ecohealthalliance.org,
 # and I'm on Twitter as @noamross
 #
-# Later, you'll be able to find the materials for this workshop here:
+# Later, you'll be able to find the materials from this workshop here:
 # Find it at https://github.com/ecohealthalliance/2019-06-06-ehn-workshop
 #
-state_0 =
